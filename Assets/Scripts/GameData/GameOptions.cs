907e52d4cab0e1760b58bc2293434abb9871047a
joa1v <jovitor86@gmail.com>
2023-11-25 15:25:23 -0300

    Changing order in layer and setting default options values

--- Assets/Scripts/GameData/GameOptions.cs
+++ Assets/Scripts/GameData/GameOptions.cs
@@ -5,12 +5,17 @@ using UnityEngine.UI;
 
 public class GameOptions : MonoBehaviour
 {
+    [Header("Default Values")]
+    [SerializeField, Range(1, 3)] private int _defaultSessionTime;
+    [SerializeField, Range(1, 5)] private int _defaultSpawnTime;
+    [Header("Sliders")]
     [SerializeField] private SliderWithText _gameSessionTimeSlider;
     [SerializeField] private SliderWithText _enemySpawnTimeSlider;
 
     private void Start()
     {
         SetCurrentOptions();
+
         _gameSessionTimeSlider.Slider.onValueChanged.AddListener(SetGameSessionTimer);
         _enemySpawnTimeSlider.Slider.onValueChanged.AddListener(SetEnemySpawnTimer);
     }
@@ -27,10 +32,30 @@ public class GameOptions : MonoBehaviour
 
     private void SetCurrentOptions()
     {
+        if (!PlayerPrefs.HasKey(PlayerPrefsKeys.GameSessionTime))
+        {
+            SetDefaultGameTime();
+        }
+
+        if (!PlayerPrefs.HasKey(PlayerPrefsKeys.EnemySpawnTime))
+        {
+            SetDefaultSpawnTime();
+        }
+
         int sessionTime = PlayerPrefs.GetInt(PlayerPrefsKeys.GameSessionTime);
         int spawnTime = PlayerPrefs.GetInt(PlayerPrefsKeys.EnemySpawnTime);
 
         _gameSessionTimeSlider.Value = sessionTime;
         _enemySpawnTimeSlider.Value = spawnTime;
+    }
+
+    private void SetDefaultGameTime()
+    {
+        SetGameSessionTimer(_defaultSessionTime);
+    }
+
+    private void SetDefaultSpawnTime()
+    {
+        SetEnemySpawnTimer(_defaultSpawnTime);
     }
 }
